9b98417c2d85c0d6dde33f77eadeef2f26683fd4
Anshu Kumar Shandilya <ashandilya64@gmail.com>
2020-05-04 09:07:24 +0530

    Movies List Designed

---
+++ app/src/main/java/com/ashandilya/popularmoviesapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.ashandilya.popularmoviesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
